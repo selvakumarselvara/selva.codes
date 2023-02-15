f7eaab1e684a6d4b220141445bcde3eefea9fc0e
selvakumarselvara <123964681+selvakumarselvara@users.noreply.github.com>
2023-02-15 22:01:15 +0530

    non parameterized constructor

---
+++ Ragu.java
@@ -0,0 +1,12 @@
+class Ragu
+{
+Ragu()
+{//default constructor
+System.out.println("BIKE IS CREATED");
+}
+public static void main(String args[])//main method
+{//calling default constructor
+Ragu b=new Ragu();
+}
+}
+
