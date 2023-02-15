64b80b82a32e3c38c18dec33ca987b3eee1a8a6a
selvakumarselvara <123964681+selvakumarselvara@users.noreply.github.com>
2023-02-15 22:03:25 +0530

    nonparameterized without return type

---
+++ Raja.java
@@ -0,0 +1,10 @@
+public class Raja
+{
+//p=principlamount,r=rate ofninterest,n=number of days
+public static void main(String args[])
+{
+double p =1000 ,n=2, r=7.5;
+double si=(p*n*r)/100;
+System.out.println("the simple interest is " +si);
+}
+}
