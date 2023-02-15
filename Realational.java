83983cbce4bce61bede8aaa1e2a6eab73a2d8875
selvakumarselvara <123964681+selvakumarselvara@users.noreply.github.com>
2023-02-15 21:53:24 +0530

    Realationaloperator

---
+++ Realational.java
@@ -0,0 +1,14 @@
+class Realational
+{
+public static void main(String args[])
+{
+int a =123,b =100;
+System.out.println(a == b);
+System.out.println(a < b);
+System.out.println(a > b);
+System.out.println(a <= b);
+System.out.println(a >= b);
+System.out.println(a != b);
+}
+
+}
