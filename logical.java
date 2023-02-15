85cdbffb0038fd67d54c540221c018dfbceac23f
selvakumarselvara <123964681+selvakumarselvara@users.noreply.github.com>
2023-02-15 21:51:28 +0530

    logicaloperator

---
+++ logical.java
@@ -0,0 +1,10 @@
+class logical
+{
+public static void main(String args[])
+{
+int a1=25,a2=50;
+System.out.println(a1 >=25 & a2 <=25);
+System.out.println(a1 >=25 | a2 >=25);
+//System.out.println(a1 ! a2);
+}
+}
