9a5cfc8b7421f9b875ed52568cd23e140f19cf96
selvakumarselvara <123964681+selvakumarselvara@users.noreply.github.com>
2023-02-15 21:42:33 +0530

    Assignment operator.java

---
+++ Assigmentoperator.java
@@ -0,0 +1,20 @@
+class Assigmentoperator
+{
+public static void main(String args[])
+{
+int a=4;
+int var;
+var = a;
+System.out.println("= variable is" +var);
+var +=a;
+System.out.println("+= variable is" +var);
+var -=a;
+System.out.println("-= variable is" +var);
+var *=a;
+System.out.println("*= variable is" +var);
+var /=a;
+System.out.println("/= variable is" +var);
+var %=a;
+System.out.println("%= variable is" +var);
+}
+}
