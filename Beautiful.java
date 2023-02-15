d104ad50eca76e20a17c75744c066506efb1375b
selvakumarselvara <123964681+selvakumarselvara@users.noreply.github.com>
2023-02-15 21:58:09 +0530

    Stringoperator

---
+++ Beautiful.java
@@ -0,0 +1,17 @@
+class Beautiful
+{
+       public static void main(String args[])
+       {
+        String name="Selva Kumar";
+        int age=22;
+        float percent=25.25f;
+        char gender='m';
+        boolean married=false;
+        System.out.println("NAME     :"+name);
+        System.out.println("AGE      :"+age);
+        System.out.println("PERCENT  :"+percent);
+        System.out.println("GENDER   :"+gender);
+        System.out.println("STATUS   :"+married);
+       }
+}
+
